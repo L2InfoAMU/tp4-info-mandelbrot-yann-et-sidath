deef73b5c30c50f9e963d5d1d6a4a8f5bde4d973
m17005981 <yoshii13@yahoo.fr>
2018-11-07 10:35:34 +0100

    UPDATE: correction des statics (ZERO, ONE, I)

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -34,18 +34,18 @@ public class Complex {
     /**
      * Zero as a complex number
      */
-    static Complex ZERO = new Complex(0.01, 0);
+    static Complex ZERO = new Complex(0, 0);
 
     /**
      * One as a complex number
      */
-    static Complex ONE = new Complex(1, 1);
+    static Complex ONE = new Complex(1, 0);
 
 
     /**
      * The complex number whose square is -1
      */
-    static Complex I = new Complex(0, -1);
+    static Complex I = new Complex(0, 1);
 
     double getReal() {
         return real;
