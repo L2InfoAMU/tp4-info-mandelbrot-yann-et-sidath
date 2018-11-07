84f07894e52295ba2527561e0c1594b57596ae8e
m17005981 <yoshii13@yahoo.fr>
2018-11-07 11:31:15 +0100

    UPDATE: correction de reciprocal

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -152,7 +152,7 @@ public class Complex {
      * @return a complex number <code>c</code> such that <code>this * c = 1</code>
      */
     Complex reciprocal() {
-        if (this.equals(ONE)){
+        if (this.equals(ZERO)){
             throw new ArithmeticException("divide by zero");
         }
         double m = squaredModulus();
